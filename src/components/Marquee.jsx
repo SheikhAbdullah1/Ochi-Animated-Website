1b4c61503ddb37d17c140a5837aca45f282a9787
Abdullah <sa4446952@gmail.com>
2024-10-14 01:40:47 +0500

    Ochi animated Website Incomplete

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+function Marquee() {
+  return (
+    <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden">
+        <motion.h1 initial={{x : 0}} animate={{x: '-100%'}} transition={{ease: 'linear'}} className="text-[17vw] leading-none font-['Founders_Grotesk_X-Condesend'] uppercase font-semibold pt-2 mb-2">
+          we are ochi
+        </motion.h1>
+        <motion.h1 initial={{x : 0}} animate={{x: '-100%'}} transition={{ease: 'linear'}} className="text-[17vw] leading-none font-['Founders_Grotesk_X-Condesend'] uppercase font-semibold pt-2 mb-2">
+          we are ochi
+        </motion.h1>
+      </div>
+    </div>
+  );
+}
+
+export default Marquee;
